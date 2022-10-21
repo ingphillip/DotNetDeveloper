f6a027e036bca199ff2f15381a82366eb389c751
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2022-10-20 21:37:43 -0500

    add record sample

---
+++ RecordsApp/Persona.cs
@@ -0,0 +1,36 @@
+﻿namespace RecordsApp;
+
+public record Persona (string Nombre, string Apellido, int Edad);
+
+public record Empleado (string Nombre, string Apellido, int Edad, string Profesion) : Persona(Nombre, Apellido, Edad);
+
+public record struct Cuenta(int id, decimal monto);
+
+public record class Empresa(string Nombre);
+
+public record class Numero
+{
+    public int Num;
+
+    public Numero(int num)
+    {
+        Num = num;
+    }
+
+    public override sealed string ToString()
+    {
+        return $"El número ingresado es {Num}";
+    }
+}
+
+public record class NumeroComodin : Numero
+{
+    public NumeroComodin(int num) : base(num)
+    {
+
+    }
+    public override sealed string ToString()
+    {
+        return $"El numero {Num} es un comodín";
+    }
+}
---
+++ RecordsApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using RecordsApp;
+
+Persona persona = new("Juan", "Perez", 35);
+
+Persona personaEditado = persona with { Nombre = "Pedro" };
+
+Empleado empleado = new("Fabian", "Peña", 25, "Arquitecto");
+
+Console.WriteLine(empleado);
+
+Persona personaOrigen = empleado;
+
+Console.WriteLine($"Hola {personaOrigen.Nombre} {personaOrigen.Apellido}, su edad es {personaOrigen.Edad} años");
+
+Console.WriteLine(personaEditado);
+
+Console.WriteLine(persona);
+
+Console.Read();
