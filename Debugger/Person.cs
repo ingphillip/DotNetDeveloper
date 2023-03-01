8ef1965ebe67e00d61491dfafd3b5b68ec0a112e
Felipe Armando Sotelo Sotelo <felipe.sotelo@live.com>
2023-03-01 10:38:17 -0500

    Add Demo to DebuggerDisplay

---
+++ Debugger/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugger
+{
+    [DebuggerDisplay("{Name + \" \" + LastName}")]
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Debugger/Program.cs
@@ -0,0 +1,9 @@
+﻿using Debugger;
+
+Person person = new Person();
+person.Name = "Felipe";
+person.LastName = "Sotelo";
+
+Console.WriteLine(person);
+
+Console.ReadLine();
